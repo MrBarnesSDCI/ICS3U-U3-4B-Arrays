a77f96976b83b5fdac7a14d9b0ba6a4bf3d89a4d
MrBarnesTVDSB <90469601+MrBarnesSDCI@users.noreply.github.com>
2021-12-03 09:20:00 -0500

    Add files via upload

---
+++ student.hpp
@@ -0,0 +1,22 @@
+#pragma once // prevents multiple definitions
+
+#include <iostream> // print library
+#include <string>
+using namespace std;
+
+// ===============
+// implementations
+
+//Not testing exercise 1, still worth doing
+
+//Replace string function
+int exerciseTwo(string arr[], int arrSize, string searchTerm)
+{
+	return -8; // TODO: fix
+}
+
+int * exerciseThree(int arrayFill, int arrSize)
+{
+	int arr[1] = {0};
+	return arr; // TODO: fix
+}
